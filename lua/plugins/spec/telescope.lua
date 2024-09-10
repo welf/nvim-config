dd63384730b79091f146095c893a0bb9a9b7bda2
Arthur Welf <arthur.welf@gmail.com>
2024-09-10 05:26:21 +0200

    Update Telescope settings

--- lua/plugins/spec/telescope.lua
+++ lua/plugins/spec/telescope.lua
@@ -1,37 +1,17 @@
 return {
   "nvim-telescope/telescope.nvim",
   lazy = false,
-  init = function()
-    local map = vim.keymap.set
-    local builtin = require "telescope.builtin"
-
-    map("n", "<leader>sa", function()
-      builtin.find_files {
-        follow = true,
-        no_ignore = true,
-        hidden = true,
-        prompt_prefix = " 󱡴  ",
-        prompt_title = "[S]earch [A]ll project files",
-      }
-    end, { desc = "Telescope search all files" })
-    map("n", "<leader>sb", "<cmd>Telescope buffers<CR>", { desc = "[S]earch [B]uffers" })
-    map("n", "<leader>sf", "<cmd>Telescope find_files<CR>", { desc = "[S]earch [F]iles" })
-    map("n", "<leader>so", "<cmd>Telescope oldfiles<CR>", { desc = "[S]earch previously [O]pened files" })
-    map("n", "<leader>sc", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "[S]earch in [C]urrent buffer" })
-    map("n", "<leader>sw", "<cmd>Telescope live_grep<CR>", { desc = "[S]earch [W]ord (live grep)" })
-    -- map("n", "<leader>ft", "<cmd>Telescope terms<CR>", { desc = "Telescope terms" })
-    -- map("n", "<leader>th", "<cmd>Telescope themes<CR>", { desc = "Telescope NvChad themes" })
-    map("n", "<leader>sr", "<cmd>Telescope lsp_references<CR>", { desc = "[S]earch References" })
-    map("n", "<leader>sm", "<cmd>Telescope marks<CR>", { desc = "[S]earch [M]arks" })
-    map("n", "<leader>sh", "<cmd>Telescope highlights<CR>", { desc = "[S]earch [H]ighlights" })
-    map("n", "<leader>sd", "<cmd>Telescope diagnostics<CR>", { desc = "[S]earch LSP [D]iagnostics" })
-    map("n", "<leader>st", "<cmd>Telescope treesitter<CR>", { desc = "[S]earch [T]reeSitter symbols" })
-    map("n", "<leader>sp", "<cmd>Telescope builtin<CR>", { desc = "[S]earch Telescope [P]ickers" })
-    map("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", { desc = "Search [G]it [C]ommits" })
-    map("n", "<leader>gs", "<cmd>Telescope git_status<CR>", { desc = "Search [G]it [S]tatus" })
-    map("n", "<leader>s?", "<cmd>Telescope help_tags<CR>", { desc = "[S]earch help tags" })
-  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-ui-select.nvim",
+    -- Useful for getting pretty icons, but requires a Nerd Font.
+    { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+  },
+  -- [[ Configure Telescope ]]
+  -- See `:help telescope` and `:help telescope.setup()`
   opts = {
+    -- You can put your default mappings / updates / etc. in here
+    --  All the info you're looking for is in `:help telescope.setup()`
     defaults = {
       selection_caret = " ",
       entry_prefix = " ",
@@ -57,5 +37,70 @@ return {
         prompt_title = "Builtin Pickers",
       },
     },
+    extensions = {
+      ['ui-select'] = {
+        require('telescope.themes').get_dropdown(),
+      },
+    },
   },
+  config = function()
+    -- Telescope is a fuzzy finder that comes with a lot of different things that
+    -- it can fuzzy find! It's more than just a "file finder", it can search
+    -- many different aspects of Neovim, your workspace, LSP, and more!
+    --
+    -- The easiest way to use Telescope, is to start by doing something like:
+    --  :Telescope help_tags
+    --
+    -- After running this command, a window will open up and you're able to
+    -- type in the prompt window. You'll see a list of `help_tags` options and
+    -- a corresponding preview of the help.
+    --
+    -- Two important keymaps to use while in Telescope are:
+    --  - Insert mode: <c-/>
+    --  - Normal mode: ?
+    --
+    -- This opens a window that shows you all of the keymaps for the current
+    -- Telescope picker. This is really useful to discover what Telescope can
+    -- do as well as how to actually do it!
+
+    -- Enable Telescope extensions if they are installed
+    pcall(require('telescope').load_extension, 'fzf')
+    pcall(require('telescope').load_extension, 'ui-select')
+
+    -- See `:help telescope.builtin`
+    local builtin = require 'telescope.builtin'
+    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+    -- Slightly advanced example of overriding default behavior and theme
+    vim.keymap.set('n', '<leader>/', function()
+      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = true,
+      })
+    end, { desc = '[/] Fuzzily search in current buffer' })
+
+    -- It's also possible to pass additional configuration options.
+    --  See `:help telescope.builtin.live_grep()` for information about particular keys
+    vim.keymap.set('n', '<leader>s/', function()
+      builtin.live_grep {
+        grep_open_files = true,
+        prompt_title = 'Live Grep in Open Files',
+      }
+    end, { desc = '[S]earch [/] in Open Files' })
+
+    -- Shortcut for searching your Neovim configuration files
+    vim.keymap.set('n', '<leader>sn', function()
+      builtin.find_files { cwd = vim.fn.stdpath 'config' }
+    end, { desc = '[S]earch [N]eovim files' })
+  end,
 }
