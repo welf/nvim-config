7c1a08bbdcdbafd75c7bc33c97a34c194709839a
Arthur Welf <arthur.welf@gmail.com>
2024-09-14 08:01:43 +0200

    Rename nvim-highlight-color plugin

---
+++ lua/aw/plugins/highlight-colors.lua
@@ -0,0 +1,9 @@
+return {
+  "brenoprata10/nvim-highlight-colors",
+  event = "VeryLazy",
+  opts = {},
+  config = function()
+    local c = require("nvim-highlight-colors")
+    c.setup({})
+  end,
+}
