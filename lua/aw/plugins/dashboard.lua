b136f6c1a52d16d8bb15ae6b7f7071b27e562679
Arthur Welf <arthur.welf@gmail.com>
2024-10-29 06:46:10 +0100

    Move persistence to dashboard deps

--- lua/aw/plugins/dashboard.lua
+++ lua/aw/plugins/dashboard.lua
@@ -4,6 +4,41 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
     "famiu/bufdelete.nvim", -- for autocmd
+    {
+      "folke/persistence.nvim",
+      event = "BufReadPre",
+      opts = {
+        options = { "buffers", "curdir", "tabpages", "winsize", "help", "blank", "terminal", "folds", "tabpages" },
+        dir = vim.fn.stdpath("state") .. "/sessions/", -- directory where session files are saved
+        -- minimum number of file buffers that need to be open to save
+        -- Set to 0 to always save
+        need = 1,
+        branch = true, -- use git branch to save session
+      },
+      keys = {
+        {
+          "<leader>qs",
+          function()
+            require("persistence").load()
+          end,
+          desc = "Restore Session",
+        },
+        {
+          "<leader>ql",
+          function()
+            require("persistence").load({ last = true })
+          end,
+          desc = "Restore Last Session",
+        },
+        {
+          "<leader>qd",
+          function()
+            require("persistence").stop()
+          end,
+          desc = "Don't Save Current Session",
+        },
+      },
+    },
   },
   opts = {
     theme = "hyper", --  theme is doom and hyper default is hyper
@@ -82,7 +117,7 @@ return {
       -- action = func(path) vim.cmd('Telescope find_files cwd=' .. path) end
       project = {
         enable = true,
-        limit = 5,
+        limit = 9,
         icon = "󰹈 ",
         label = "Recent Projects",
         action = "Telescope find_files cwd=",
