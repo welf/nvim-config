302e488946df1206ac2b0b02f0d7cf564a4278e7
Arthur Welf <arthur.welf@gmail.com>
2026-04-22 04:08:28 +0200

    Defer actions-preview telescope theme to config phase
    get_dropdown() can't be called in the spec-eval scope because telescope
    may not be loaded yet; move it into config() and wrap <leader>cp in a
    function so require is also deferred.

--- lua/aw/plugins/actions-preview.lua
+++ lua/aw/plugins/actions-preview.lua
@@ -23,22 +23,18 @@ return {
     backend = { "telescope", "nui" },
 
     -- options related to telescope.nvim
-    telescope = vim.tbl_extend(
-      "force",
-      -- telescope theme: https://github.com/nvim-telescope/telescope.nvim#themes
-      require("telescope.themes").get_dropdown(),
+    telescope = {
+      -- telescope theme will be set in config function
       -- a table for customizing content
-      {
-        -- a function to make a table containing the values to be displayed.
-        -- fun(action: Action): { title: string, client_name: string|nil }
-        make_value = nil,
+      -- a function to make a table containing the values to be displayed.
+      -- fun(action: Action): { title: string, client_name: string|nil }
+      make_value = nil,
 
-        -- a function to make a function to be used in `display` of a entry.
-        -- see also `:h telescope.make_entry` and `:h telescope.pickers.entry_display`.
-        -- fun(values: { index: integer, action: Action, title: string, client_name: string }[]): function
-        make_make_display = nil,
-      }
-    ),
+      -- a function to make a function to be used in `display` of a entry.
+      -- see also `:h telescope.make_entry` and `:h telescope.pickers.entry_display`.
+      -- fun(values: { index: integer, action: Action, title: string, client_name: string }[]): function
+      make_make_display = nil,
+    },
 
     -- options for nui.nvim components
     nui = {
@@ -76,20 +72,25 @@ return {
     },
   },
   config = function()
+    local telescope_themes = require("telescope.themes")
     require("actions-preview").setup({
-      telescope = {
-        sorting_strategy = "ascending",
-        layout_strategy = "vertical",
-        layout_config = {
-          width = 0.8,
-          height = 0.9,
-          prompt_position = "top",
-          preview_cutoff = 20,
-          preview_height = function(_, _, max_lines)
-            return max_lines - 15
-          end,
-        },
-      },
+      telescope = vim.tbl_extend(
+        "force",
+        telescope_themes.get_dropdown(),
+        {
+          sorting_strategy = "ascending",
+          layout_strategy = "vertical",
+          layout_config = {
+            width = 0.8,
+            height = 0.9,
+            prompt_position = "top",
+            preview_cutoff = 20,
+            preview_height = function(_, _, max_lines)
+              return max_lines - 15
+            end,
+          },
+        }
+      ),
     })
   end,
 }
